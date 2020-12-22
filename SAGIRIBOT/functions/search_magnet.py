cea08b9d27c28435c8821a51ca4d41fa607f4630
SAGIRI <shijiaduyifan@126.com>
2020-12-22 22:32:28 +0800

    fix bug

--- SAGIRIBOT/functions/search_magnet.py
+++ SAGIRIBOT/functions/search_magnet.py
@@ -11,7 +11,7 @@ from SAGIRIBOT.data_manage.get_data.get_setting import get_setting
 from SAGIRIBOT.functions.get_proxy import get_proxy
 
 
-async def search_magnet(keyword: str, group_id: int) -> list:
+async def search_magnet_old(keyword: str, group_id: int) -> list:
     url = f"https://btsow.com/search/{keyword}"
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
@@ -37,7 +37,6 @@ async def search_magnet(keyword: str, group_id: int) -> list:
                 Plain(text="Proxy Error!")
             ])
         ]
-
 
     # print("html:", html)
     if not html:
@@ -66,7 +65,9 @@ async def search_magnet(keyword: str, group_id: int) -> list:
                 async with session.get(url=record["link"]) as resp:
                     magnet_html = await resp.text()
             # print(magnet_html)
-            record["magnet"] = re.findall(r'<textarea id="magnetLink" name="magnetLink" class="magnet-link hidden-xs" readonly>(.*?)</textarea>', magnet_html, re.S)[0]
+            record["magnet"] = re.findall(
+                r'<textarea id="magnetLink" name="magnetLink" class="magnet-link hidden-xs" readonly>(.*?)</textarea>',
+                magnet_html, re.S)[0]
             record["title"] = div.find("a").find("div", {"class": "file"}).get_text()
             record["size"] = div.find("div", {"class": "size"}).get_text()
             record["date"] = div.find("div", {"class": "date"}).get_text()
@@ -107,3 +108,87 @@ async def search_magnet(keyword: str, group_id: int) -> list:
             ])
         ]
 
+
+async def search_magnet(keyword: str, group_id: int, audio: bool = False, video: bool = False, app: bool = False,
+                  games: bool = False, porn: bool = False):
+    args = "100,200,300,400,500"
+    if not audio:
+        args = args.replace("100,", "")
+    if not video:
+        args = args.replace("200,", "")
+    if not app:
+        args = args.replace("300,", "")
+    if not games:
+        args = args.replace("400,", "")
+    if not porn:
+        if len(args) == 3:
+            args = "0"
+        else:
+            args = args.replace(",500", "")
+
+    url = f"https://tpb.party/search/{keyword}/1/99/{args}"
+    print(url)
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url=url, headers=headers) as resp:
+            html = await resp.text()
+    soup = BeautifulSoup(html, "html.parser")
+    data = soup.find("table").find_all("tr")
+    divs = []
+    for i in data:
+        div = i.find("div", {"class": "detName"})
+        if div:
+            if div and div.find("a").get_text() not in divs:
+                divs.append(div)
+    records = []
+    count = 0
+    for i in divs:
+        count += 1
+        if count > 5:
+            break
+        record = {"title": i.get_text().strip()}
+        print(record["title"])
+        record["href"] = i.find("a", href=True)["href"]
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=record["href"], headers=headers) as resp:
+                html_torrent = await resp.text()
+
+        soup_torrent = BeautifulSoup(html_torrent, "html.parser")
+        record["magnet"] = soup_torrent.find("div", {"class": "download"}).find_all("a", href=True)[0]["href"]
+        records.append(record)
+
+    text = "--------------------\n搜索到结果：\n"
+    for data in records:
+        text += f"标题：{data['title']}\n"
+        text += f"磁力：{data['magnet']}\n"
+        text += "--------------------\n"
+
+    long_text_setting = await get_setting(group_id, "longTextType")
+    if long_text_setting == "img":
+        img = text2piiic(string=text, poster="", length=max(len(x) for x in text.split("\n")))
+        img.save("./statics/temp/tempMagnet.png")
+        return [
+            "quoteSource",
+            MessageChain.create([
+                Plain(text="为防止tx吞超长文本及封禁此账号，已将链接转为图片格式，请自行使用OCR（有毅力的也可以手打）获取链接！\n"),
+                Image.fromLocalFile("./statics/temp/tempMagnet.png")
+            ])
+        ]
+    elif long_text_setting == "text":
+        return [
+            "quoteSource",
+            MessageChain.create([
+                Plain(text=text)
+            ])
+        ]
+    else:
+        return [
+            "None",
+            MessageChain.create([
+                Plain(text="数据库 longTextType 项出错！请检查！")
+            ])
+        ]
