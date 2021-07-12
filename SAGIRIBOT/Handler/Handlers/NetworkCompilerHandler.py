509a0dda25a1e39a1d3f72a5f7ba15703b04f5a4
SAGIRI-kawaii <shijiaduyifan@126.com>
2021-07-12 17:17:31 +0800

    fix bug

--- SAGIRIBOT/Handler/Handlers/NetworkCompilerHandler.py
+++ SAGIRIBOT/Handler/Handlers/NetworkCompilerHandler.py
@@ -39,7 +39,7 @@ class NetworkCompilerHandler(AbstractHandler):
             await update_user_call_count_plus1(group, member, UserCalledCount.functions, "functions")
             if not await get_setting(group.id, Setting.compile):
                 return MessageItem(MessageChain.create([Plain(text="网络编译器功能关闭了呐~去联系管理员开启吧~")]), Normal(GroupStrategy()))
-            language = re.findall(r"super (.*?):", message_text, re.S)[0]
+            language = re.findall(r"super (.*?)", message_text, re.S)[0]
             code = message_text[8 + len(language):]
             result = await NetworkCompilerHandler.network_compiler(group, member, language, code)
             if isinstance(result, str):
