eeb3429f399019dd3d61d73ea5124b01894bd177
SAGIRI-kawaii <shijiaduyifan@126.com>
2021-08-03 22:51:34 +0800

    添加随机老婆功能

--- SAGIRIBOT/Handler/Handlers/RandomWifeHandler.py
+++ SAGIRIBOT/Handler/Handlers/RandomWifeHandler.py
@@ -1,3 +1,5 @@
+import random
+
 from graia.saya import Saya, Channel
 from graia.application import GraiaMiraiApplication
 from graia.application.message.chain import MessageChain
@@ -9,27 +11,32 @@ from SAGIRIBOT.Handler.Handler import AbstractHandler
 from SAGIRIBOT.MessageSender.MessageItem import MessageItem
 from SAGIRIBOT.MessageSender.MessageSender import GroupMessageSender
 from SAGIRIBOT.decorators import frequency_limit_require_weight_free
-from SAGIRIBOT.MessageSender.Strategy import GroupStrategy, Normal, QuoteSource
+from SAGIRIBOT.MessageSender.Strategy import GroupStrategy, QuoteSource
 
 saya = Saya.current()
 channel = Channel.current()
 
 
 @channel.use(ListenerSchema(listening_events=[GroupMessage]))
-async def template_handler(app: GraiaMiraiApplication, message: MessageChain, group: Group, member: Member):
-    if result := await TemplateHandler.handle(app, message, group, member):
+async def random_wife_handler(app: GraiaMiraiApplication, message: MessageChain, group: Group, member: Member):
+    if result := await RandomWifeHandler.handle(app, message, group, member):
         await GroupMessageSender(result.strategy).send(app, result.message, message, group, member)
 
 
-class TemplateHandler(AbstractHandler):
-    __name__ = "TemplateHandler"
-    __description__ = "Handler示例"
+class RandomWifeHandler(AbstractHandler):
+    __name__ = "RandomWifeHandler"
+    __description__ = "随机老婆"
     __usage__ = "None"
 
     @staticmethod
     async def handle(app: GraiaMiraiApplication, message: MessageChain, group: Group, member: Member):
-        expression = None
-        if expression:
-            return MessageItem(MessageChain.create([Plain(text="正常发送示例")]), Normal(GroupStrategy()))
-        else:
-            return MessageItem(MessageChain.create([Plain(text="回复示例")]), QuoteSource(GroupStrategy()))
+        if message.asDisplay() in ("来个老婆", "随机老婆"):
+            return await RandomWifeHandler.get_random_wife()
+
+    @staticmethod
+    @frequency_limit_require_weight_free(4)
+    async def get_random_wife():
+        return MessageItem(
+            MessageChain.create([Image.fromNetworkAddress(f"https://www.thiswaifudoesnotexist.net/example-{random.randint(1, 100000)}.jpg")]),
+            QuoteSource(GroupStrategy())
+        )
