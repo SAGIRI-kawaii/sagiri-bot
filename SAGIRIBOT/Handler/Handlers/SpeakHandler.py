59884a055ce65a1f2a00fc9ba48d53dd010aa120
nullqwertyuiop <49110142+nullqwertyuiop@users.noreply.github.com>
2021-08-14 11:13:55 +0800

    Update SpeakHandler.py

--- SAGIRIBOT/Handler/Handlers/SpeakHandler.py
+++ SAGIRIBOT/Handler/Handlers/SpeakHandler.py
@@ -1,4 +1,6 @@
 import time
+import json
+import uuid
 import random
 import string
 import base64
@@ -14,11 +16,20 @@ from graia.saya.builtins.broadcast.schema import ListenerSchema
 from SAGIRIBOT.MessageSender.MessageSender import GroupMessageSender
 from graia.application.event.messages import Group, Member, GroupMessage
 
+from SAGIRIBOT.ORM.AsyncORM import UserCalledCount
 from SAGIRIBOT.decorators import switch, blacklist
 from SAGIRIBOT.utils import get_config, get_tx_sign
 from SAGIRIBOT.Handler.Handler import AbstractHandler
+from SAGIRIBOT.utils import update_user_call_count_plus1
 from SAGIRIBOT.MessageSender.MessageItem import MessageItem
 from SAGIRIBOT.MessageSender.Strategy import GroupStrategy, Normal, QuoteSource
+
+from tencentcloud.common import credential
+from tencentcloud.common.profile.client_profile import ClientProfile
+from tencentcloud.common.profile.http_profile import HttpProfile
+from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
+from tencentcloud.tts.v20190823 import tts_client, models
+
 
 saya = Saya.current()
 channel = Channel.current()
@@ -40,6 +51,7 @@ class SpeakHandler(AbstractHandler):
     @blacklist()
     async def handle(app: GraiaMiraiApplication, message: MessageChain, group: Group, member: Member):
         if message.asDisplay().startswith("说 "):
+            await update_user_call_count_plus1(group, member, UserCalledCount.functions, "functions")
             text = ''.join([plain.text for plain in message.get(Plain)])[2:].replace(" ", '，')
             voice = await SpeakHandler.get_voice(text)
             if isinstance(voice, str):
@@ -50,31 +62,25 @@ class SpeakHandler(AbstractHandler):
 
     @staticmethod
     async def get_voice(text: str) -> Union[str, bytes]:
-        url = "https://api.ai.qq.com/fcgi-bin/aai/aai_tts"
-        text = text.strip()
-        app_id = get_config("txAppId")
-        t = time.time()
-        time_stamp = str(int(t))
-        nonce_str = ''.join(random.sample(string.ascii_letters + string.digits, 10))
-
-        params = {
-            'app_id': app_id,
-            'time_stamp': time_stamp,
-            'nonce_str': nonce_str,
-            "speaker": '7',
-            "format": '3',
-            "volume": '0',
-            "speed": "100",
-            "text": text,
-            "aht": '0',
-            "apc": "58"
-        }
-        sign = await get_tx_sign(params)
-        params["sign"] = sign
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url=url, params=params) as resp:
-                res = await resp.json()
-        if res["ret"] > 0:
-            print(res)
-            return f"Error:{res['msg']}"
-        return base64.b64decode(res["data"]["speech"])
+        try:
+            user_data = get_config("tencent")
+            cred = credential.Credential(user_data["secretId"], user_data["secretKey"])
+            session_ID = str(uuid.uuid4())
+            httpProfile = HttpProfile()
+            httpProfile.endpoint = "tts.tencentcloudapi.com"
+            clientProfile = ClientProfile()
+            clientProfile.httpProfile = httpProfile
+            client = tts_client.TtsClient(cred, "ap-guangzhou", clientProfile)
+            req = models.TextToVoiceRequest()
+            params = {
+                "Text": text,
+                "SessionId": session_ID,
+                "ModelType": 1
+            }
+            req.from_json_string(json.dumps(params))
+            resp = client.TextToVoice(req)
+            voice = json.loads(resp.to_json_string())["Audio"]
+            return base64.b64decode(voice)
+        except TencentCloudSDKException as err:
+            logger.error(traceback.format_exc())
+            return str(err)
