d3212684d5ab0a9469d003368fd8f0b2a7f85118
SAGIRI-kawaii <shijiaduyifan@126.com>
2022-02-12 22:01:44 +0800

    添加哔咔插件

---
+++ sagiri_bot/handler/handlers/pica/Pica.py
@@ -0,0 +1,242 @@
+import os
+import hmac
+import time
+import json
+import uuid
+import shutil
+import aiohttp
+import zipfile
+from io import BytesIO
+from pathlib import Path
+from urllib import parse
+from hashlib import sha256
+from PIL import Image as IMG
+from aiohttp import TCPConnector
+from typing import Literal, Optional, Tuple, Union
+
+from sagiri_bot.core.app_core import AppCore
+
+BASE_PATH = os.path.dirname(__file__)
+CACHE_PATH = BASE_PATH + "/cache/download"
+global_url = "https://picaapi.picacomic.com/"
+api_key = "C69BAF41DA5ABD1FFEDC6D2FEA56B"
+uuid_s = str(uuid.uuid4()).replace("-", "")
+header = {
+        "api-key": "C69BAF41DA5ABD1FFEDC6D2FEA56B",
+        "app-channel": "2",
+        "app-version": "2.2.1.3.3.4",
+        "app-uuid": "defaultUuid",
+        "image-quality": "original",
+        "app-platform": "android",
+        "app-build-version": "44",
+        "Content-Type": "application/json; charset=UTF-8",
+        "User-Agent": "okhttp/3.8.1",
+        "accept": "application/vnd.picacomic.com.v1+json",
+        "time": 0,
+        "nonce": "",
+        "signature": "encrypt",
+}
+path_filter = ['\\', '/', ':', '*', '?', '"', '<', '>', '|']
+
+core = AppCore.get_core_instance()
+config = core.get_config()
+proxy = config.proxy if config.proxy != "proxy" else ''
+pica_config = config.functions.get("pica")
+username = pica_config.get("username", None)
+password = pica_config.get("password", None)
+loop = core.get_loop()
+DOWNLOAD_CACHE = pica_config.get("download_cache", True)
+
+
+class Pica:
+
+    def __init__(self, account, password):
+        if not os.path.exists(CACHE_PATH):
+            os.makedirs(CACHE_PATH)
+        self.account = account
+        self.password = password
+        self.header = header.copy()
+        self.header["nonce"] = uuid_s
+        loop.run_until_complete(self.check())
+
+    async def check(self):
+        token = await self.login()
+        self.header["authorization"] = token
+
+    def update_signature(self, url: str, method: Literal["GET", "POST"]) -> dict:
+        ts = str(int(time.time()))
+        temp_header = self.header.copy()
+        temp_header["time"] = ts
+        temp_header["signature"] = self.encrypt(url, ts, method)
+        if method == "GET":
+            temp_header.pop("Content-Type")
+        return temp_header
+
+    @staticmethod
+    def encrypt(url, ts, method):
+        datas = [
+            global_url, url.replace(global_url, ""),
+            ts,
+            uuid_s,
+            method,
+            "C69BAF41DA5ABD1FFEDC6D2FEA56B",
+            "2.2.1.3.3.4",
+            "45"
+        ]
+        _src = Pica.__ConFromNative(datas)
+        _key = Pica.__SigFromNative()
+        return Pica.HashKey(_src, _key)
+
+    @staticmethod
+    def __ConFromNative(datas):
+        return str(datas[1]) + str(datas[2]) + str(datas[3]) + str(datas[4]) + str(datas[5])
+
+    @staticmethod
+    def __SigFromNative():
+        return '~d}$Q7$eIni=V)9\\RK/P.RM4;9[7|@/CA}b~OW!3?EV`:<>M7pddUBL5n|0/*Cn'
+
+    @staticmethod
+    def HashKey(src, key):
+        app_secret = key.encode('utf-8')  # 秘钥
+        data = src.lower().encode('utf-8')  # 数据
+        signature = hmac.new(app_secret, data, digestmod=sha256).hexdigest()
+        return signature
+
+    async def login(self):
+        """ 登录获取token """
+        api = "auth/sign-in"
+        url = global_url + api
+        send = {"email": self.account, "password": self.password}
+        temp_header = self.update_signature(url, "POST")
+        async with aiohttp.ClientSession(connector=TCPConnector(ssl=False)) as session:
+            async with session.post(url=url, headers=temp_header, data=json.dumps(send), proxy=proxy) as resp:
+                self.header["authorization"] = (await resp.json())["data"]["token"]
+        return self.header["authorization"]
+
+    async def categories(self):
+        """ 获取所有目录 """
+        api = "categories"
+        url = global_url + api
+        temp_header = self.update_signature(url, "GET")
+        async with aiohttp.ClientSession(connector=TCPConnector(ssl=False)) as session:
+            async with session.get(url=url, headers=temp_header, proxy=proxy) as resp:
+                return (await resp.json())["data"]["categories"]
+
+    async def search(self, keyword: str):
+        """ 关键词搜索 """
+        api = global_url + "comics/search?page={0}" + "&q={0}".format(parse.quote(keyword))
+        url = api.format(1)
+        temp_header = self.update_signature(url, "GET")
+        async with aiohttp.ClientSession(connector=TCPConnector(ssl=False)) as session:
+            async with session.get(url=url, headers=temp_header, proxy=proxy) as resp:
+                __pages = (await resp.json())["data"]["comics"]["pages"]
+            _return = []
+            for _ in range(1, 3):
+                url = api.format(_)
+                temp_header = self.update_signature(url, "GET")
+                async with session.get(url=url, headers=temp_header, proxy=proxy) as resp:
+                    __res = (await resp.json())["data"]["comics"]["docs"]
+                    for __ in __res:
+                        if __["likesCount"] < 200:
+                            continue
+                        if __["pagesCount"] / __["epsCount"] > 60 or __["epsCount"] > 10:
+                            continue
+                        _return.append({"name": __["title"], "id": __["_id"]})
+        return _return
+
+    async def random(self):
+        """ 随机本子 """
+        url = global_url + "comics/random"
+        temp_header = self.update_signature(url, "GET")
+        async with aiohttp.ClientSession(connector=TCPConnector(ssl=False)) as session:
+            async with session.get(url=url, headers=temp_header, proxy=proxy) as resp:
+                return (await resp.json())["data"]["comics"]
+
+    async def rank(self, tt: Literal["H24", "D7", "D30"] = "H24"):
+        """ 排行榜 """
+        url = global_url + f"comics/leaderboard?ct=VC&tt={tt}"
+        temp_header = self.update_signature(url, "GET")
+        async with aiohttp.ClientSession(connector=TCPConnector(ssl=False)) as session:
+            async with session.get(url=url, headers=temp_header, proxy=proxy) as resp:
+                return (await resp.json())["data"]["comics"]
+
+    async def comic_info(self, book_id: str):
+        """ 漫画详情 """
+        url = global_url + f"comics/{book_id}"
+        temp_header = self.update_signature(url, "GET")
+        async with aiohttp.ClientSession(connector=TCPConnector(ssl=False)) as session:
+            async with session.get(url=url, headers=temp_header, proxy=proxy) as resp:
+                return (await resp.json())["data"]["comic"]
+
+    async def download_image(self, url: str, path: Optional[Union[str, Path]] = None) -> bytes:
+        temp_header = self.update_signature(url, "GET")
+        async with aiohttp.ClientSession(connector=TCPConnector(ssl=False)) as session:
+            async with session.get(url=url, headers=temp_header, proxy=proxy) as resp:
+                if resp.status != 200:
+                    resp.raise_for_status()
+                image_bytes = await resp.read()
+                if not path:
+                    return image_bytes
+                else:
+                    IMG.open(BytesIO(image_bytes)).save(str(path))
+                    return image_bytes
+
+    async def download_comic(self, book_id: str, return_zip: bool = True) -> Optional[Tuple[str, Union[str, bytes]]]:
+        info = await self.comic_info(book_id)
+        episodes = info["epsCount"]
+        comic_name = f"{info['title']} - {info['author']}"
+        for char in path_filter:
+            comic_name = comic_name.replace(char, ' ')
+        comic_path = CACHE_PATH + f"/{comic_name}"
+        if not os.path.exists(comic_path):
+            os.mkdir(comic_path)
+        for episode in range(episodes):
+            url = global_url + f"comics/{book_id}/order/{episode + 1}/pages"
+            temp_header = self.update_signature(url, "GET")
+            async with aiohttp.ClientSession(connector=TCPConnector(ssl=False)) as session:
+                async with session.get(url=url, headers=temp_header, proxy=proxy) as resp:
+                    data = (await resp.json())["data"]
+            episode_title = data["ep"]["title"]
+            episode_path = comic_path + f"/{episode_title}"
+            if not os.path.exists(episode_path):
+                os.mkdir(episode_path)
+            for img in data["pages"]["docs"]:
+                media = img["media"]
+                img_url = f"{media['fileServer']}/static/{media['path']}"
+                image_path = episode_path + f"/{media['originalName']}"
+                print(comic_name, episode_title, media['originalName'], sep=" - ")
+                if os.path.exists(image_path):
+                    continue
+                _ = await self.download_image(img_url, image_path)
+        if return_zip:
+            return self.zip_directory(comic_path, comic_name)
+        else:
+            return comic_path, comic_name
+
+    @staticmethod
+    def zip_file(path: str, zip_name: str, password: str = "i_luv_sagiri") -> Optional[Tuple[str, bytes]]:
+        shutil.make_archive(f"{path}/{zip_name}", 'zip', path)
+        with open(Path(path) / f"{zip_name}.zip", "rb") as r:
+            return zip_name, r.read()
+
+    @staticmethod
+    def zip_directory(path, zip_name) -> Optional[Tuple[str, bytes]]:
+        with zipfile.ZipFile(Path(path) / f"{zip_name}.zip", mode='w') as zipf:
+            len_dir_path = len(path)
+            for root, _, files in os.walk(path):
+                for file in files:
+                    if file[:-3] == "zip":
+                        continue
+                    file_path = os.path.join(root, file)
+                    zipf.write(file_path, file_path[len_dir_path:])
+        with open(Path(path) / f"{zip_name}.zip", "rb") as r:
+            return zip_name, r.read()
+
+
+pica = Pica(username, password)
+# print(loop.run_until_complete(pica.search("SAGIRI")))
+# print(loop.run_until_complete(pica.categories()))
+# print(loop.run_until_complete(pica.random()))
+# print(loop.run_until_complete(pica.rank()))
+# print(loop.run_until_complete(pica.comic_info("5ce4d819431b5d017ddc8199")))
+# loop.run_until_complete(pica.download_comic("5821a1d55f6b9a4f93ef4a6b"))
---
+++ sagiri_bot/handler/handlers/pica/__init__.py
@@ -0,0 +1,327 @@
+import os
+from loguru import logger
+from datetime import datetime, timedelta
+
+from graia.saya import Saya, Channel
+from graia.ariadne.app import Ariadne
+from graia.ariadne.model import UploadMethod
+from graia.ariadne.exception import RemoteException
+from graia.ariadne.message.chain import MessageChain
+from graia.saya.builtins.broadcast.schema import ListenerSchema
+from graia.ariadne.event.message import Group, Member, GroupMessage
+from graia.ariadne.message.parser.twilight import Twilight, Sparkle
+from graia.ariadne.message.element import ForwardNode, Image, Plain, Forward
+from graia.ariadne.message.parser.twilight import RegexMatch, FullMatch, UnionMatch, ArgumentMatch
+
+from .Pica import pica
+from sagiri_bot.core.app_core import AppCore
+from sagiri_bot.utils import MessageChainUtils
+from sagiri_bot.decorators import switch, blacklist
+from sagiri_bot.handler.handler import AbstractHandler
+from utils.daily_number_limiter import DailyNumberLimiter
+from sagiri_bot.message_sender.message_item import MessageItem
+from sagiri_bot.message_sender.message_sender import MessageSender
+from sagiri_bot.message_sender.strategy import Normal, QuoteSource
+from sagiri_bot.decorators import frequency_limit_require_weight_free
+
+saya = Saya.current()
+channel = Channel.current()
+
+channel.name("Pica")
+channel.author("SAGIRI-kawaii")
+channel.description(
+    "一个接入哔咔漫画的插件，支持搜索关键词，随机漫画，下载漫画，排行榜获取\n"
+    "在群中发送 `pica search {keyword}` 来搜索特定关键词\n"
+    "在群中发送 `pica random` 来获取随机漫画\n"
+    "在群中发送 `pica rank -H24/-D7/-D30` 来获取24小时/一周/一月内排行榜\n"
+    "在群中发送 `pica download (-message|-forward) {comic_id}` 来获取图片消息/转发消息/压缩文件形式的漫画"
+)
+
+core = AppCore.get_core_instance()
+config = core.get_config()
+bot_qq = config.bot_qq
+DOWNLOAD_CACHE = config.functions["pica"]["download_cache"]
+SEARCH_CACHE = config.functions["pica"]["search_cache"]
+
+BASE_PATH = os.path.dirname(__file__)
+SEARCH_CACHE_PATH = BASE_PATH + "/cache/search"
+
+if not os.path.exists(SEARCH_CACHE_PATH):
+    os.makedirs(SEARCH_CACHE_PATH)
+
+DAILY_DOWNLOAD_LIMIT = int(config.functions["pica"]["daily_download_limit"])
+DAILY_SEARCH_LIMIT = int(config.functions["pica"]["daily_search_limit"])
+DAILY_RANDOM_LIMIT = int(config.functions["pica"]["daily_random_limit"])
+DAILY_RANK_LIMIT = int(config.functions["pica"]["daily_rank_limit"])
+
+DAILY_DOWNLOAD_LIMITER = DailyNumberLimiter(DAILY_DOWNLOAD_LIMIT)
+DAILY_SEARCH_LIMITER = DailyNumberLimiter(DAILY_SEARCH_LIMIT)
+DAILY_RANDOM_LIMITER = DailyNumberLimiter(DAILY_RANDOM_LIMIT)
+DAILY_RANK_LIMITER = DailyNumberLimiter(DAILY_RANK_LIMIT)
+
+limit_text = {
+    "download": "今天已经达到每日下载上限啦~\n明天再来吧~\n❤你❤这❤个❤小❤色❤批❤~",
+    "search": "今天已经达到每日搜索上限啦~\n明天再来吧~\n想找本子的话，自己去下载个哔咔好咯~",
+    "random": "今天已经达到每日随机本子上限啦~\n明天再来吧~\n你这个人对本子真是挑剔呢~",
+    "rank": "今天已经达到每日排行榜查询上限啦~\n明天再来吧~\n排行榜一次还看不够嘛~"
+}
+
+
+@channel.use(
+    ListenerSchema(
+        listening_events=[GroupMessage],
+        inline_dispatchers=[
+            Twilight(
+                Sparkle(
+                    [FullMatch("pica")],
+                    {
+                        "operation": UnionMatch("download", "search", "random", "rank"),
+                        "forward_type": ArgumentMatch("-forward", action="store_true", optional=True),
+                        "message_type": ArgumentMatch("-message", action="store_true", optional=True),
+                        "rank_time": UnionMatch("-H24", "-D7", "-D30", optional=True),
+                        "content": RegexMatch(r".+", optional=True)
+                    }
+                )
+            )
+        ]
+    )
+)
+async def pica_function(
+    app: Ariadne,
+    message: MessageChain,
+    group: Group,
+    member: Member,
+    operation: UnionMatch,
+    message_type: ArgumentMatch,
+    forward_type: ArgumentMatch,
+    rank_time: UnionMatch,
+    content: RegexMatch
+):
+    if result := await PicaHandler.handle(
+        app, message, group, member, operation, message_type, forward_type, rank_time, content
+    ):
+        await MessageSender(result.strategy).send(app, result.message, message, group, member)
+
+
+class PicaHandler(AbstractHandler):
+    __name__ = "PicaHandler"
+    __description__ = "一个接入哔咔漫画的插件，支持搜索关键词，随机漫画，下载漫画，排行榜获取"
+    __usage__ = "在群中发送 `pica search {keyword}` 来搜索特定关键词\n" \
+                "在群中发送 `pica random` 来获取随机漫画\n" \
+                "在群中发送 `pica rank -H24/-D7/-D30` 来获取24小时/一周/一月内排行榜\n" \
+                "在群中发送 `pica download (-message|-forward) {comic_id}` 来获取图片消息/转发消息/压缩文件形式的漫画"
+
+    @staticmethod
+    @switch()
+    @blacklist()
+    @frequency_limit_require_weight_free(9)
+    async def handle(
+        app: Ariadne,
+        message: MessageChain,
+        group: Group,
+        member: Member,
+        operation: UnionMatch,
+        message_type: ArgumentMatch,
+        forward_type: ArgumentMatch,
+        rank_time: UnionMatch,
+        content: RegexMatch
+    ):
+        if any([
+            operation.result.asDisplay() == "download" and not DAILY_DOWNLOAD_LIMITER.check(member.id),
+            operation.result.asDisplay() == "search" and not DAILY_SEARCH_LIMITER.check(member.id),
+            operation.result.asDisplay() == "random" and not DAILY_RANDOM_LIMITER.check(member.id),
+            operation.result.asDisplay() == "rank" and not DAILY_RANK_LIMITER.check(member.id)
+        ]):
+            return MessageItem(
+                MessageChain(limit_text[str(operation.result.asDisplay())]), QuoteSource()
+            )
+
+        if operation.result.asDisplay() == "download":
+            DAILY_DOWNLOAD_LIMITER.increase(member.id)
+        elif operation.result.asDisplay() == "search":
+            DAILY_SEARCH_LIMITER.increase(member.id)
+        elif operation.result.asDisplay() == "random":
+            DAILY_RANDOM_LIMITER.increase(member.id)
+        elif operation.result.asDisplay() == "rank":
+            DAILY_RANK_LIMITER.increase(member.id)
+
+        if operation.result.asDisplay() == "download" and forward_type.matched and content.matched:
+            comic_id = content.result.asDisplay()
+            await app.sendMessage(group, MessageChain(f"收到请求，正在下载{comic_id}..."))
+            info = await pica.download_comic(comic_id, False)
+            image_list = []
+            for root, _, files in os.walk(info[0]):
+                for file in files:
+                    if file[-4:] in (".jpg", ".png"):
+                        image_list.append(os.path.join(root, file))
+            node_count = 0
+            time_count = 0
+            time_base = datetime.now() - timedelta(seconds=len(image_list))
+            forward_nodes = [ForwardNode(
+                senderId=bot_qq,
+                time=time_base + timedelta(seconds=time_count),
+                senderName="纱雾酱",
+                messageChain=MessageChain.create(Plain("IOS系统可能会乱序，请参照下方文件名和发送时间顺序自行排序！"))
+            )]
+            for path in image_list:
+                node_count += 1
+                time_count += 1
+                forward_nodes.append(
+                    ForwardNode(
+                        senderId=bot_qq,
+                        time=time_base + timedelta(seconds=time_count),
+                        senderName="纱雾酱",
+                        messageChain=MessageChain.create(
+                            Image(path=path),
+                            Plain(f"\n{path.replace(info[0], '')}\n{time_base + timedelta(seconds=time_count)}")
+                        )
+                    )
+                )
+                if node_count == 20:
+                    await app.sendMessage(group, MessageChain.create(Forward(nodeList=forward_nodes)))
+                    forward_nodes = [
+                        ForwardNode(
+                            senderId=bot_qq,
+                            time=time_base + timedelta(seconds=time_count),
+                            senderName="纱雾酱",
+                            messageChain=MessageChain.create(Plain("IOS系统可能会乱序，请参照下方文件名和发送时间顺序自行排序！"))
+                        )
+                    ]
+                    node_count = 0
+            return MessageItem(MessageChain.create(Forward(nodeList=forward_nodes)), Normal())
+
+        elif operation.result.asDisplay() == "download" and message_type.matched and content.matched:
+            comic_id = content.result.asDisplay()
+            await app.sendMessage(group, MessageChain(f"收到请求，正在下载{comic_id}..."))
+            info = await pica.download_comic(comic_id, False)
+            image_list = []
+            for root, _, files in os.walk(info[0]):
+                for file in files:
+                    if not file[-3:] == "zip":
+                        image_list.append(os.path.join(root, file))
+            return MessageItem(MessageChain.create([Image(path=path) for path in image_list]), Normal())
+
+        elif operation.result.asDisplay() == "download" and content.matched:
+            comic_id = message.asDisplay()[14:]
+            await app.sendMessage(group, MessageChain(f"收到请求，正在下载{comic_id}..."))
+            info = await pica.download_comic(comic_id)
+            try:
+                await app.uploadFile(
+                    data=info[1],
+                    method=UploadMethod.Group,
+                    target=group,
+                    name=f"{info[0].replace(' ', '')}"
+                )
+            except RemoteException:
+                await app.uploadFile(
+                    data=info[1],
+                    method=UploadMethod.Group,
+                    target=group,
+                    name=f"pica_{comic_id}"
+                )
+            return MessageItem(MessageChain("请自行添加 .zip 后缀名后进行解压"), Normal())
+
+        elif operation.result.asDisplay() in ("search", "random"):
+            search = operation.result.asDisplay() == "search"
+            keyword = content.result.asDisplay() if content.matched else ''
+            if search and content.matched:
+                await app.sendMessage(group, MessageChain(f"收到请求，正在搜索{keyword}..."))
+            data = (await pica.search(keyword))[:10] \
+                if search else (await pica.random())[:10]
+            forward_nodes = []
+            for comic in data:
+                comic_info = await pica.comic_info(comic["id"]) if operation.result.asDisplay() == "search" else comic
+                try:
+                    forward_nodes.append(
+                        ForwardNode(
+                            senderId=bot_qq,
+                            time=datetime.now(),
+                            senderName="纱雾酱",
+                            messageChain=(await MessageChainUtils.messagechain_to_img(
+                                MessageChain.create([
+                                    await PicaHandler.get_thumb(comic_info),
+                                    Plain(text=f"\n名称：{comic_info['title']}\n"),
+                                    Plain(text=f"作者：{comic_info['author']}\n"),
+                                    Plain(text=f"描述：{comic_info['description']}\n") if search else Plain(text=''),
+                                    Plain(text=f"分类：{'、'.join(comic_info['categories'])}\n"),
+                                    Plain(text=f"标签：{'、'.join(comic_info['tags'])}\n") if search else Plain(text=''),
+                                    Plain(text=f"页数：{comic_info['pagesCount']}\n"),
+                                    Plain(text=f"章节数：{comic_info['epsCount']}\n"),
+                                    Plain(text=f"完结状态：{'已完结' if comic_info['finished'] else '未完结'}\n"),
+                                    Plain(text=f"喜欢: {comic_info['totalLikes']}    "),
+                                    Plain(text=f"浏览次数: {comic_info['totalViews']}    ")
+                                ]), max_width=2160
+                            )).extend([
+                                Plain(text="\n发送下列命令下载：\n"),
+                                Plain(text=f"转发消息形式：pica download -forward {comic_info['_id']}\n"),
+                                Plain(text=f"消息图片形式：pica download -message {comic_info['_id']}\n"),
+                                Plain(text=f"压缩包形式：pica download {comic_info['_id']}")
+                            ])
+                        )
+                    )
+                except Exception as e:
+                    logger.error(e)
+                    continue
+            return MessageItem(MessageChain.create(Forward(nodeList=forward_nodes)), Normal())
+
+        elif operation.result.asDisplay() == "rank":
+            rank_time = rank_time.result.asDisplay() if rank_time.matched else "-H24"
+            if rank_time not in ("-H24", "-D7", "-D30"):
+                return MessageItem(
+                    MessageChain.create([
+                        Plain(text="错误的时间！支持的选项：\n"),
+                        Plain(text="H24：24小时排行榜\n"),
+                        Plain(text="D7：一周排行榜\n"),
+                        Plain(text="D30：一月排行榜\n"),
+                        Plain(text="命令格式：pica random -{time}")
+                    ]),
+                    QuoteSource()
+                )
+            data = (await pica.rank(rank_time[1:]))[:10]
+            forward_nodes = []
+            rank = 0
+            for comic_info in data:
+                rank += 1
+                try:
+                    forward_nodes.append(
+                        ForwardNode(
+                            senderId=bot_qq,
+                            time=datetime.now(),
+                            senderName="纱雾酱",
+                            messageChain=(await MessageChainUtils.messagechain_to_img(
+                                MessageChain.create([
+                                    await PicaHandler.get_thumb(comic_info),
+                                    Plain(text=f"\n排名：{rank}\n"),
+                                    Plain(text=f"名称：{comic_info['title']}\n"),
+                                    Plain(text=f"作者：{comic_info['author']}\n"),
+                                    Plain(text=f"分类：{'、'.join(comic_info['categories'])}\n"),
+                                    Plain(text=f"页数：{comic_info['pagesCount']}\n"),
+                                    Plain(text=f"章节数：{comic_info['epsCount']}\n"),
+                                    Plain(text=f"完结状态：{'已完结' if comic_info['finished'] else '未完结'}\n"),
+                                    Plain(text=f"喜欢: {comic_info['totalLikes']}    "),
+                                    Plain(text=f"浏览次数: {comic_info['totalViews']}    ")
+                                ]), max_width=2160
+                            )).extend([
+                                Plain(text="\n发送下列命令下载：\n"),
+                                Plain(text=f"转发消息形式：pica download -forward {comic_info['_id']}\n"),
+                                Plain(text=f"消息图片形式：pica download -message {comic_info['_id']}\n"),
+                                Plain(text=f"压缩包形式：pica download {comic_info['_id']}")
+                            ])
+                        )
+                    )
+                except Exception as e:
+                    logger.error(e)
+                    continue
+            return MessageItem(MessageChain.create(Forward(nodeList=forward_nodes)), Normal())
+
+    @staticmethod
+    async def get_thumb(comic_info: dict) -> Image:
+        if os.path.exists(f"{SEARCH_CACHE_PATH}/{comic_info['_id']}.jpg"):
+            return Image(path=f"{SEARCH_CACHE_PATH}/{comic_info['_id']}.jpg")
+        else:
+            return Image(
+                data_bytes=await pica.download_image(
+                    url=f"{comic_info['thumb']['fileServer']}/static/{comic_info['thumb']['path']}",
+                    path=f"{SEARCH_CACHE_PATH}/{comic_info['_id']}.jpg" if SEARCH_CACHE else None
+                )
+            )
