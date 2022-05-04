f963788eaf18f9e2dad73a36777f5981df8f46b0
nullqwertyuiop <49110142+nullqwertyuiop@users.noreply.github.com>
2022-05-05 00:16:32 +0800

    Update repeater.py

--- sagiri_bot/handler/handlers/repeater.py
+++ sagiri_bot/handler/handlers/repeater.py
@@ -1,65 +1,72 @@
-from typing import Optional
-from asyncio import Semaphore
-
-from graia.saya import Saya, Channel
 from graia.ariadne.app import Ariadne
+from graia.ariadne.event.message import Group, GroupMessage, ActiveGroupMessage
 from graia.ariadne.message.chain import MessageChain
+from graia.ariadne.message.element import (Image, Plain, At, Quote, AtAll, Face, Poke,
+                                           Forward, App, Json, Xml, MarketFace)
+from graia.saya import Saya, Channel
 from graia.saya.builtins.broadcast.schema import ListenerSchema
-from graia.ariadne.event.message import Group, Member, GroupMessage
 
-from sagiri_bot.utils import group_setting
+from sagiri_bot.control import Function
 from sagiri_bot.orm.async_orm import Setting
-from sagiri_bot.decorators import switch, blacklist
-from sagiri_bot.message_sender.strategy import Normal
-from sagiri_bot.handler.handler import AbstractHandler
-from sagiri_bot.message_sender.message_item import MessageItem
+from sagiri_bot.utils import group_setting
 
 saya = Saya.current()
 channel = Channel.current()
 
 channel.name("Repeater")
-channel.author("SAGIRI-kawaii")
+channel.author("SAGIRI-kawaii, nullqwertyuiop")
 channel.description("一个复读插件，有两条以上相同信息时自动触发")
 
-mutex = Semaphore(1)
+group_repeat = {}
 
 
-@channel.use(ListenerSchema(listening_events=[GroupMessage]))
-async def repeater(app: Ariadne, message: MessageChain, group: Group, member: Member):
-    if result := await Repeater.handle(app, message, group, member):
-        await app.sendMessage(group, result.message)
-
-
-class Repeater(AbstractHandler):
-    """
-    复读Handler
-    """
-    __name__ = "Repeater"
-    __description__ = "一个复读插件"
-    __usage__ = "有两条以上相同信息时自动触发"
-
-    group_repeat = {}
-    
-    @staticmethod
-    @switch()
-    @blacklist()
-    async def handle(app: Ariadne, message: MessageChain, group: Group, member: Member) -> Optional[MessageItem]:
-        group_id = group.id
-        message_serialization = message.asPersistentString()
-        if await group_setting.get_setting(group_id, Setting.repeat):
-            if group_id in Repeater.group_repeat.keys():
-                # await mutex.acquire()
-                Repeater.group_repeat[group.id]["lastMsg"] = Repeater.group_repeat[group.id]["thisMsg"]
-                Repeater.group_repeat[group.id]["thisMsg"] = message_serialization
-                if Repeater.group_repeat[group.id]["lastMsg"] != Repeater.group_repeat[group.id]["thisMsg"]:
-                    Repeater.group_repeat[group.id]["stopMsg"] = ""
-                    # mutex.release()
-                else:
-                    if Repeater.group_repeat[group.id]["thisMsg"] != Repeater.group_repeat[group.id]["stopMsg"]:
-                        Repeater.group_repeat[group.id]["stopMsg"] = Repeater.group_repeat[group.id]["thisMsg"]
-                        # mutex.release()
-                        return MessageItem(message.asSendable(), Normal())
+@channel.use(
+    ListenerSchema(
+        listening_events=[GroupMessage],
+        decorators=[Function.require(channel.module, log=False)]
+    )
+)
+async def repeater_handler(app: Ariadne, message: MessageChain, group: Group):
+    if (not await group_setting.get_setting(group, Setting.repeat)
+            or not await group_setting.get_setting(group, Setting.switch)):
+        return
+    global group_repeat
+    if message.has(Forward) or message.has(App) or message.has(Json) or message.has(Xml) or message.has(MarketFace):
+        group_repeat[group.id] = {
+            "msg": message.asPersistentString(),
+            "count": -1
+        }
+        return
+    message_serialization = message.asPersistentString()
+    if group.id not in group_repeat.keys():
+        group_repeat[group.id] = {"msg": message_serialization, "count": 1}
+    else:
+        if message_serialization == group_repeat[group.id]["msg"]:
+            if group_repeat[group.id]["count"] == -1:
+                return
+            count = group_repeat[group.id]["count"] + 1
+            if count == 2:
+                group_repeat[group.id]["count"] = count
+                msg = message.include(Plain, Image, At, Quote, AtAll, Face, Poke)
+                if msg.asDisplay() == "<! 不支持的消息类型 !>":
+                    group_repeat[group.id] = {
+                        "msg": msg.asPersistentString(),
+                        "count": -1
+                    }
+                    return
+                return await app.sendGroupMessage(group, msg.asSendable())
             else:
-                Repeater.group_repeat[group_id] = {"lastMsg": "", "thisMsg": message_serialization, "stopMsg": ""}
+                group_repeat[group.id]["count"] = count
+                return
+        else:
+            group_repeat[group.id]["msg"] = message_serialization
+            group_repeat[group.id]["count"] = 1
 
-        return None
+
+@channel.use(ListenerSchema(listening_events=[ActiveGroupMessage]))
+async def repeater_flush_handler(event: ActiveGroupMessage):
+    global group_repeat
+    group_repeat[event.subject.id] = {
+        "msg": event.messageChain.asPersistentString(),
+        "count": -1
+        }
