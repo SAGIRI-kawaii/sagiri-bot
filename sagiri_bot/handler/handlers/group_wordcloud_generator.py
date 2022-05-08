4ef366ca49fdb04ba416758e382ba21f24a494d2
nullqwertyuiop <49110142+nullqwertyuiop@users.noreply.github.com>
2022-05-08 23:37:09 +0800

    Update group_wordcloud_generator.py

--- sagiri_bot/handler/handlers/group_wordcloud_generator.py
+++ sagiri_bot/handler/handlers/group_wordcloud_generator.py
@@ -1,81 +1,91 @@
 import os
 import random
-import numpy as np
-import jieba.analyse
-
-from io import BytesIO
-from PIL import Image as IMG
 from datetime import datetime
+from io import BytesIO
+
+import jieba.analyse
 import matplotlib.pyplot as plt
-from sqlalchemy import select, func
+import numpy as np
+from PIL import Image as IMG
 from dateutil.relativedelta import relativedelta
+from graia.ariadne.app import Ariadne
+from graia.ariadne.event.message import Group, Member, GroupMessage
+from graia.ariadne.message.chain import MessageChain
+from graia.ariadne.message.element import Plain, Image, Source
+from graia.ariadne.message.parser.twilight import (
+    Twilight,
+    UnionMatch,
+    FullMatch,
+    MatchResult,
+)
+from graia.saya import Saya, Channel
+from graia.saya.builtins.broadcast.schema import ListenerSchema
+from sqlalchemy import select, func
 from wordcloud import WordCloud, ImageColorGenerator
 
-from graia.saya import Saya, Channel
-from graia.ariadne.app import Ariadne
-from graia.ariadne.message.chain import MessageChain
-from graia.ariadne.message.element import Plain, Image
-from graia.saya.builtins.broadcast.schema import ListenerSchema
-from graia.ariadne.event.message import Group, Member, GroupMessage
-
+from sagiri_bot.control import (
+    FrequencyLimit,
+    Function,
+    BlackListControl,
+    UserCalledCountControl,
+)
+from sagiri_bot.orm.async_orm import ChatRecord
 from sagiri_bot.orm.async_orm import orm
-from sagiri_bot.handler.handler import AbstractHandler
-from sagiri_bot.message_sender.strategy import QuoteSource
-from sagiri_bot.message_sender.message_item import MessageItem
-from sagiri_bot.orm.async_orm import ChatRecord, UserCalledCount
-from sagiri_bot.message_sender.message_sender import MessageSender
-from sagiri_bot.utils import update_user_call_count_plus, user_permission_require
-from sagiri_bot.decorators import frequency_limit_require_weight_free, switch, blacklist
-
+from sagiri_bot.utils import user_permission_require
 
 saya = Saya.current()
 channel = Channel.current()
 
 channel.name("GroupWordCloudGenerator")
 channel.author("SAGIRI-kawaii")
-channel.description(
-    "群词云生成器，"
-    "在群中发送 `[我的|本群][月|年]内总结` 即可查看个人/群 月/年词云（群词云需要权限等级2）"
+channel.description("群词云生成器，" "在群中发送 `[我的|本群]([月|年]内|今日)总结` 即可查看个人/群 月/年词云（群词云需要权限等级2）")
+
+
+@channel.use(
+    ListenerSchema(
+        listening_events=[GroupMessage],
+        inline_dispatchers=[
+            Twilight(
+                [
+                    UnionMatch("我的", "本群") @ "scope",
+                    UnionMatch("年内", "月内", "今日") @ "period",
+                    FullMatch("总结"),
+                ]
+            )
+        ],
+        decorators=[
+            FrequencyLimit.require(channel.meta["name"], 3),
+            Function.require(channel.module),
+            BlackListControl.enable(),
+            UserCalledCountControl.add(UserCalledCountControl.FUNCTIONS),
+        ],
+    )
 )
+async def group_wordcloud_generator(
+    app: Ariadne,
+    message: MessageChain,
+    group: Group,
+    member: Member,
+    scope: MatchResult,
+    period: MatchResult,
+):
+    scope = "group" if scope.result.asDisplay() == "本群" else "member"
+    if scope == "group" and not await user_permission_require(group, member, 2):
+        return await app.sendGroupMessage(
+            group,
+            MessageChain.create([Plain(text="权限不足呢~爪巴!")]),
+            quote=message.getFirst(Source),
+        )
+
+    period = period.result.asDisplay()
+    await app.sendGroupMessage(
+        group,
+        await GroupWordCloudGenerator.get_review(group, member, period, scope),
+        quote=message.getFirst(Source),
+    )
 
 
-@channel.use(ListenerSchema(listening_events=[GroupMessage]))
-async def group_wordcloud_generator(app: Ariadne, message: MessageChain, group: Group, member: Member):
-    if result := await GroupWordCloudGenerator.handle(app, message, group, member):
-        await MessageSender(result.strategy).send(app, result.message, message, group, member)
-
-
-class GroupWordCloudGenerator(AbstractHandler):
-    __name__ = "GroupWordCloudGenerator"
-    __description__ = "群词云生成器"
-    __usage__ = "在群中发送 `我的月/年内总结` 即可查看个人月/年词云\n在群中发送 `本群月/年内总结` 即可查看群组月/年词云（需要权限等级2）"
-
-    @staticmethod
-    @switch()
-    @blacklist()
-    async def handle(app: Ariadne, message: MessageChain, group: Group, member: Member):
-        message_text = message.asDisplay()
-        if message_text == "我的月内总结":
-            await update_user_call_count_plus(group, member, UserCalledCount.functions, "functions")
-            return await GroupWordCloudGenerator.get_review(group, member, "month", "member")
-        elif message_text == "我的年内总结":
-            await update_user_call_count_plus(group, member, UserCalledCount.functions, "functions")
-            return await GroupWordCloudGenerator.get_review(group, member, "year", "member")
-        elif message_text == "本群月内总结":
-            if await user_permission_require(group, member, 2):
-                await update_user_call_count_plus(group, member, UserCalledCount.functions, "functions")
-                return await GroupWordCloudGenerator.get_review(group, member, "month", "group")
-            else:
-                return MessageItem(MessageChain.create([Plain(text="权限不足呢~爪巴!")]), QuoteSource())
-        elif message_text == "本群年内总结":
-            if await user_permission_require(group, member, 2):
-                await update_user_call_count_plus(group, member, UserCalledCount.functions, "functions")
-                return await GroupWordCloudGenerator.get_review(group, member, "year", "group")
-            else:
-                return MessageItem(MessageChain.create([Plain(text="权限不足呢~爪巴!")]), QuoteSource())
-        else:
-            return None
-
+class GroupWordCloudGenerator:
     @staticmethod
     async def count_words(sp, n):
         w = {}
@@ -91,7 +101,21 @@ class GroupWordCloudGenerator(AbstractHandler):
     @staticmethod
     async def filter_label(label_list: list) -> list:
         not_filter = ["草"]
-        filter_list = ["jpg", "png", "img-", '{', '}', '<', '>', "url", "pid", "p0", "www", ":/", "qq"]
+        filter_list = [
+            "jpg",
+            "png",
+            "img-",
+            "{",
+            "}",
+            "<",
+            ">",
+            "url",
+            "pid",
+            "p0",
+            "www",
+            ":/",
+            "qq",
+        ]
         image_filter = "mirai:"
         result = []
         for i in label_list:
@@ -103,28 +127,25 @@ class GroupWordCloudGenerator(AbstractHandler):
                 continue
             elif i in not_filter:
                 result.append(i)
-            elif len(i) != 1 and i.find('nbsp') < 0:
+            elif len(i) != 1 and i.find("nbsp") < 0:
                 result.append(i)
         return result
 
     @staticmethod
     async def draw_word_cloud(read_name) -> bytes:
-
         def random_pic(base_path: str) -> str:
             path_dir = os.listdir(base_path)
             path = random.sample(path_dir, 1)[0]
             return base_path + path
 
-        mask = np.array(IMG.open(random_pic(f'statics/wordcloud/')))
-        # print(mask.shape)
+        mask = np.array(IMG.open(random_pic(f"statics/wordcloud/")))
         wc = WordCloud(
-            font_path=f'statics/fonts/STKAITI.TTF',
-            background_color='white',
-            # max_words=500,
+            font_path=f"statics/fonts/STKAITI.TTF",
+            background_color="white",
             max_font_size=100,
             width=1920,
             height=1080,
-            mask=mask
+            mask=mask,
         )
         name = []
         value = []
@@ -134,83 +155,88 @@ class GroupWordCloudGenerator(AbstractHandler):
         for i in range(len(name)):
             name[i] = str(name[i])
         dic = dict(zip(name, value))
-        # print(dic)
-        # print(len(dic.keys()))
         wc.generate_from_frequencies(dic)
         image_colors = ImageColorGenerator(mask, default_color=(255, 255, 255))
-        # print(image_colors.image.shape)
         wc.recolor(color_func=image_colors)
         plt.imshow(wc.recolor(color_func=image_colors), interpolation="bilinear")
         plt.axis("off")
         bytes_io = BytesIO()
         img = wc.to_image()
-        img.save(bytes_io, format='PNG')
+        img.save(bytes_io, format="PNG")
         return bytes_io.getvalue()
 
     @staticmethod
-    @frequency_limit_require_weight_free(3)
-    async def get_review(group: Group, member: Member, review_type: str, target: str) -> MessageItem:
+    async def get_review(
+        group: Group, member: Member, review_type: str, target: str
+    ) -> MessageChain:
         group_id = group.id
         member_id = member.id
         time = datetime.now()
         time_right = time.strftime("%Y-%m-%d %H:%M:%S")
-        if review_type == "year":
+        if review_type == "年内":
             timep = time - relativedelta(years=1)
             time_left = (time - relativedelta(years=1)).strftime("%Y-%m-%d %H:%M:%S")
-            tag = "年内"
-        elif review_type == "month":
+        elif review_type == "月内":
             timep = time - relativedelta(months=1)
             time_left = (time - relativedelta(months=1)).strftime("%Y-%m-%d %H:%M:%S")
-            tag = "月内"
         else:
-            return MessageItem(MessageChain.create([Plain(text="Error: review_type invalid!")]), QuoteSource())
+            timep = time - relativedelta(days=1)
+            time_left = (time - relativedelta(days=1)).strftime("%Y-%m-%d %H:%M:%S")
 
         sql = select(
             ChatRecord.id,
             ChatRecord.time,
             ChatRecord.group_id,
             ChatRecord.member_id,
-            ChatRecord.seg
+            ChatRecord.seg,
         ).where(
             ChatRecord.group_id == group_id,
             ChatRecord.member_id == member_id if target == "member" else True,
             ChatRecord.time < time,
-            ChatRecord.time > timep
+            ChatRecord.time > timep,
         )
 
         if not (res := list(await orm.fetchall(sql))):
-            return MessageItem(MessageChain.create([Plain(text="没有你的发言记录呐~")]), QuoteSource())
+            return MessageChain.create([Plain(text="没有你的发言记录呐~")])
         texts = []
         for i in res:
             if i[4]:
                 texts += await GroupWordCloudGenerator.filter_label(i[4].split("|"))
-        sql = select([func.count()]).select_from(
-            ChatRecord
-        ).where(
-            ChatRecord.group_id == group_id,
-            ChatRecord.member_id == member_id if target == "member" else True,
-            ChatRecord.time < time,
-            ChatRecord.time > timep
+        sql = (
+            select([func.count()])
+            .select_from(ChatRecord)
+            .where(
+                ChatRecord.group_id == group_id,
+                ChatRecord.member_id == member_id if target == "member" else True,
+                ChatRecord.time < time,
+                ChatRecord.time > timep,
+            )
         )
         if not (res := list(await orm.fetchone(sql))):
-            return MessageItem(MessageChain.create([Plain(text="没有你的发言记录呐~")]), QuoteSource())
+            return MessageChain.create([Plain(text="没有你的发言记录呐~")])
 
         times = res[0]
-        return MessageItem(
-            MessageChain.create([
+        return MessageChain.create(
+            [
                 Plain(text="记录时间：\n"),
                 Plain(text=f"{time_left}"),
                 Plain(text="\n---------至---------\n"),
                 Plain(text=f"{time_right}"),
                 Plain(
-                    text=f"\n自有记录以来，{'你' if target == 'member' else '本群'}一共发了{times}条消息\n下面是{'你的' if target == 'member' else '本群的'}{tag}词云:\n"),
+                    text="\n自有记录以来，"
+                    f"{'你' if target == 'member' else '本群'}"
+                    f"一共发了{times}条消息\n下面是"
+                    f"{'你的' if target == 'member' else '本群的'}"
+                    f"{review_type}词云:\n"
+                ),
                 Image(
                     data_bytes=await GroupWordCloudGenerator.draw_word_cloud(
-                        jieba.analyse.extract_tags(" ".join(texts), topK=1000, withWeight=True, allowPOS=())
+                        jieba.analyse.extract_tags(
+                            " ".join(texts), topK=1000, withWeight=True, allowPOS=()
+                        )
                     )
-                )
-            ]),
-            QuoteSource()
+                ),
+            ]
         )
 
 
@@ -223,7 +249,7 @@ class TfIdf:
     def add_document(self, doc_name, list_of_words):
         doc_dict = {}
         for w in list_of_words:
-            doc_dict[w] = doc_dict.get(w, 0.) + 1.0
+            doc_dict[w] = doc_dict.get(w, 0.0) + 1.0
             self.corpus_dict[w] = self.corpus_dict.get(w, 0.0) + 1.0
 
         length = float(len(list_of_words))
@@ -248,7 +274,8 @@ class TfIdf:
             for k in query_dict:
                 if k in doc_dict:
                     score += (query_dict[k] / self.corpus_dict[k]) + (
-                      doc_dict[k] / self.corpus_dict[k])
+                        doc_dict[k] / self.corpus_dict[k]
+                    )
             sims.append([doc[0], score])
 
         return sims
