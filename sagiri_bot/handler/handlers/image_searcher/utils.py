ebc39113849f279330c5aa4f63e45d9d62e80a10
SAGIRI-kawaii <shijiaduyifan@126.com>
2022-05-20 22:39:24 +0800

    更新搜图插件

---
+++ sagiri_bot/handler/handlers/image_searcher/utils.py
@@ -0,0 +1,20 @@
+import functools
+
+from graia.ariadne import get_running
+from graia.ariadne.adapter import Adapter
+from graia.ariadne.message.chain import MessageChain
+
+
+async def get_thumb(url: str) -> bytes:
+    async with get_running(Adapter).session.get(url, proxy="http://localhost:7890") as resp:
+        return await resp.read()
+
+
+def error_catcher(func):
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        try:
+            return await func(*args, **kwargs)
+        except Exception as e:
+            return MessageChain(f"{func.__name__}运行出错：{str(e)}")
+    return wrapper
