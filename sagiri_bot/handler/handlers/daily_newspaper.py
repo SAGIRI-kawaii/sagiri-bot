e31d5295c98982687157c85e34866367c16090cf
SAGIRI-kawaii <shijiaduyifan@126.com>
2022-05-20 22:38:25 +0800

    添加群组获取方法

--- sagiri_bot/handler/handlers/daily_newspaper.py
+++ sagiri_bot/handler/handlers/daily_newspaper.py
@@ -7,13 +7,15 @@ from loguru import logger
 
 from graia.saya import Saya, Channel
 from graia.ariadne.app import Ariadne
-from graia.ariadne.model import Friend
+from graia.ariadne import get_running
+from graia.ariadne.adapter import Adapter
+from graia.ariadne.model import Friend, Group
 from graia.scheduler.timers import crontabify
 from graia.ariadne.message.element import Image
 from graia.ariadne.message.chain import MessageChain
-from graia.ariadne.event.message import FriendMessage
 from graia.scheduler.saya.schema import SchedulerSchema
 from graia.saya.builtins.broadcast.schema import ListenerSchema
+from graia.ariadne.event.message import FriendMessage, GroupMessage
 from graia.ariadne.message.parser.twilight import Twilight, FullMatch
 
 from sagiri_bot.utils import group_setting
@@ -26,7 +28,11 @@ host_qq = AppCore.get_core_instance().get_config().host_qq
 
 channel.name("DailyNewspaper")
 channel.author("SAGIRI-kawaii")
-channel.description("一个定时发送每日日报的插件\n主人私聊bot发送 `发送早报` 可在群中发送早报")
+channel.description(
+    "一个定时发送每日日报的插件\n"
+    "主人私聊bot发送 `发送早报` 可在群中发送早报\n"
+    "在群中发送 `今日早报` 可在群中发送早报"
+)
 
 
 @channel.use(SchedulerSchema(crontabify("30 8 * * *")))
@@ -46,18 +52,27 @@ async def main(app: Ariadne, friend: Friend):
     await send_newspaper(app)
 
 
+@channel.use(
+    ListenerSchema(
+        listening_events=[GroupMessage],
+        inline_dispatchers=[Twilight([FullMatch("今日早报")])]
+    )
+)
+async def main(app: Ariadne, group: Group):
+    await app.sendMessage(group, MessageChain([Image(data_bytes=await get_image())]))
+
+
 async def send_newspaper(app: Ariadne):
+    image_content = None
     for i in range(3):
         try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get("http://api.2xb.cn/zaob") as resp:
-                    image_url = (await resp.json()).get("imageUrl", None)
-                async with session.get(image_url) as resp:
-                    image_content = await resp.read()
+            image_content = await get_image()
             break
         except Exception as e:
             logger.error(f"第 {i + 1} 次日报加载失败\n{e}")
             await asyncio.sleep(3)
+    if not image_content:
+        return logger.error("日报获取失败！")
     for group in await app.getGroupList():
         if not await group_setting.get_setting(group, Setting.daily_newspaper):
             continue
@@ -66,3 +81,11 @@ async def send_newspaper(app: Ariadne):
         except AccountMuted:
             continue
         await asyncio.sleep(random.randint(3, 6))
+
+
+async def get_image() -> bytes:
+    async with get_running(Adapter).session.get("http://api.2xb.cn/zaob") as resp:
+        image_url = (await resp.json()).get("imageUrl", None)
+    async with get_running(Adapter).session.get(image_url) as resp:
+        image_content = await resp.read()
+    return image_content
