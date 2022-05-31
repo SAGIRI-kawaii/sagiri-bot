508391e77fb2636af3a899ff7e13a5140c4c84d4
SAGIRI-kawaii <shijiaduyifan@126.com>
2022-05-31 22:58:18 +0800

    重写黑白草图插件

--- sagiri_bot/handler/handlers/black_white_grass.py
+++ sagiri_bot/handler/handlers/black_white_grass.py
@@ -5,18 +5,14 @@ from graia.saya import Saya, Channel
 from graia.ariadne.app import Ariadne
 from graia.ariadne.message.element import Image
 from graia.ariadne.message.chain import MessageChain
+from graia.ariadne.message.parser.twilight import Twilight
+from graia.ariadne.event.message import Group, GroupMessage
 from graia.saya.builtins.broadcast.schema import ListenerSchema
-from graia.ariadne.event.message import Group, Member, GroupMessage
-from graia.ariadne.message.parser.twilight import Twilight, Sparkle
 from graia.ariadne.message.parser.twilight import RegexMatch, FullMatch, ElementMatch, RegexResult, ElementResult
 
 from sagiri_bot.utils import BuildImage
 from sagiri_bot.core.app_core import AppCore
-from sagiri_bot.decorators import switch, blacklist
-from sagiri_bot.handler.handler import AbstractHandler
-from sagiri_bot.message_sender.strategy import QuoteSource
-from sagiri_bot.message_sender.message_item import MessageItem
-from sagiri_bot.message_sender.message_sender import MessageSender
+from sagiri_bot.control import FrequencyLimit, Function, BlackListControl, UserCalledCountControl
 
 
 saya = Saya.current()
@@ -25,6 +21,7 @@ channel = Channel.current()
 channel.name("BWGrass")
 channel.author("SAGIRI-kawaii")
 channel.description("一个生成黑白草图的插件，在群中发送 `黑白[草]图 内容 图片` 即可")
+channel.meta["origin"] = "https://github.com/HibiKier/zhenxun_bot"
 
 core = AppCore.get_core_instance()
 config = core.get_config()
@@ -38,62 +35,40 @@ config = core.get_config()
                 FullMatch("黑白"), FullMatch("草", optional=True), FullMatch("图"),
                 RegexMatch(r".+") @ "content", ElementMatch(Image) @ "image"
             ])
+        ],
+        decorators=[
+            FrequencyLimit.require("black_white_grass", 1),
+            Function.require(channel.module),
+            BlackListControl.enable(),
+            UserCalledCountControl.add(UserCalledCountControl.FUNCTIONS)
         ]
     )
 )
-async def black_white_grass(
-    app: Ariadne,
-    message: MessageChain,
-    group: Group,
-    member: Member,
-    content: RegexResult,
-    image: ElementResult
-):
-    if result := await BWGrass.handle(app, message, group, member, content, image):
-        await MessageSender(result.strategy).send(app, result.message, message, group, member)
-
-
-class BWGrass(AbstractHandler):
-    __name__ = "BWGrass"
-    __description__ = "一个生成黑白草图的插件"
-    __usage__ = "在群中发送 `黑白[草]图 内容 图片` 即可"
-    __origin__ = "https://github.com/HibiKier/zhenxun_bot"
-
-    @staticmethod
-    @switch()
-    @blacklist()
-    async def handle(
-        app: Ariadne,
-        message: MessageChain,
-        group: Group,
-        member: Member,
-        content: RegexResult,
-        image: ElementResult
-    ):
-        msg = content.result.asDisplay()
-        img = await image.result.get_bytes()
-        msg = await get_translate(msg)
-        w2b = BuildImage(0, 0, background=BytesIO(img))
-        w2b.convert("L")
-        msg_sp = msg.split("<|>")
-        w, h = w2b.size
-        add_h, font_size = init_h_font_size(h)
-        bg = BuildImage(w, h + add_h, color="black", font_size=font_size)
-        bg.paste(w2b)
-        chinese_msg = formalization_msg(msg)
-        if not bg.check_font_size(chinese_msg):
-            if len(msg_sp) == 1:
-                centered_text(bg, chinese_msg, add_h)
-            else:
-                centered_text(bg, chinese_msg + "<|>" + msg_sp[1], add_h)
-        elif not bg.check_font_size(msg_sp[0]):
-            centered_text(bg, msg, add_h)
+async def black_white_grass(app: Ariadne, group: Group, content: RegexResult, image: ElementResult):
+    msg = content.result.asDisplay()
+    img = await image.result.get_bytes()
+    msg = await get_translate(msg)
+    w2b = BuildImage(0, 0, background=BytesIO(img))
+    w2b.convert("L")
+    msg_sp = msg.split("<|>")
+    w, h = w2b.size
+    add_h, font_size = init_h_font_size(h)
+    bg = BuildImage(w, h + add_h, color="black", font_size=font_size)
+    bg.paste(w2b)
+    chinese_msg = formalization_msg(msg)
+    if not bg.check_font_size(chinese_msg):
+        if len(msg_sp) == 1:
+            centered_text(bg, chinese_msg, add_h)
         else:
-            ratio = (bg.getsize(msg_sp[0])[0] + 20) / bg.w
-            add_h = add_h * ratio
-            bg.resize(ratio)
-            centered_text(bg, msg, add_h)
-        return MessageItem(MessageChain.create([Image(data_bytes=bg.pic2bytes())]), QuoteSource())
+            centered_text(bg, chinese_msg + "<|>" + msg_sp[1], add_h)
+    elif not bg.check_font_size(msg_sp[0]):
+        centered_text(bg, msg, add_h)
+    else:
+        ratio = (bg.getsize(msg_sp[0])[0] + 20) / bg.w
+        add_h = add_h * ratio
+        bg.resize(ratio)
+        centered_text(bg, msg, add_h)
+    await app.sendGroupMessage(group, MessageChain([Image(data_bytes=bg.pic2bytes())]))
 
 
 def centered_text(img: BuildImage, text: str, add_h: int):
@@ -140,10 +115,7 @@ async def get_translate(msg: str) -> str:
 def formalization_msg(msg: str) -> str:
     rst = ""
     for i in range(len(msg)):
-        if is_chinese(msg[i]):
-            rst += msg[i] + " "
-        else:
-            rst += msg[i]
+        rst += msg[i] + " " if is_chinese(msg[i]) else msg[i]
         if i + 1 < len(msg) and is_chinese(msg[i + 1]) and msg[i].isalpha():
             rst += " "
     return rst
@@ -165,7 +137,4 @@ def is_chinese(word: str) -> bool:
     参数：
         :param word: 文本
     """
-    for ch in word:
-        if not "\u4e00" <= ch <= "\u9fff":
-            return False
-    return True
+    return all("\u4e00" <= ch <= "\u9fff" for ch in word)
