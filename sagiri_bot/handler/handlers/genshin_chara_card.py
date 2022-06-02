ec58b1b99ca896c4b243ef2eb6662e042d8c5a0b
SAGIRI-kawaii <shijiaduyifan@126.com>
2022-06-02 15:09:57 +0800

    添加原神角色卡代理、依赖信息

--- sagiri_bot/handler/handlers/genshin_chara_card.py
+++ sagiri_bot/handler/handlers/genshin_chara_card.py
@@ -14,6 +14,7 @@ from graia.saya.builtins.broadcast.schema import ListenerSchema
 from graia.ariadne.message.parser.twilight import FullMatch, RegexMatch, RegexResult
 
 from utils.browser import get_browser
+from sagiri_bot.core.app_core import AppCore
 from sagiri_bot.control import FrequencyLimit, Function, BlackListControl, UserCalledCountControl
 
 saya = Saya.current()
@@ -22,6 +23,9 @@ channel = Channel.current()
 channel.name("GenshinCharaCard")
 channel.author("SAGIRI-kawaii")
 channel.description("一个原神角色卡查询插件，在群中发送 `/原神角色卡 UID 角色名` 即可")
+
+proxy = AppCore.get_core_instance().get_config().proxy
+proxy = proxy if proxy != "proxy" else None
 
 characters = {}
 
@@ -32,7 +36,7 @@ characters = {}
         inline_dispatchers=[
             Twilight([
                 FullMatch("/原神角色卡 "),
-                RegexMatch(r"1[0-9]{8}") @ "uid",
+                RegexMatch(r"[12][0-9]{8}") @ "uid",
                 RegexMatch(r".*") @ "chara"
             ])
         ],
@@ -56,48 +60,60 @@ async def genshin_chara_card(app: Ariadne, group: Group, source: Source, uid: Re
     if chara_pinyin not in characters:
         return await app.sendGroupMessage(group, MessageChain(f"角色列表中未找到角色：{chara}，请检查拼写"))
     url = f"https://enka.shinshin.moe/u/{uid}"
-    browser = await get_browser()
+    if proxy:
+        browser = await get_browser(proxy={"server": proxy})
+    else:
+        browser = await get_browser()
     if not browser:
         return await app.sendGroupMessage(group, MessageChain("获取browser失败！"))
-    page = await browser.new_page()
-    await page.goto(url, wait_until="networkidle", timeout=100000)
-    await page.set_viewport_size({"width": 2560, "height": 1080})
-    await page.evaluate("document.getElementsByClassName('Dropdown-list')[0].children[13].dispatchEvent(new Event('click'));")
-    html = await page.inner_html(".CharacterList")
-    soup = BeautifulSoup(html, "html.parser")
-    styles = [figure["style"] for figure in soup.find_all("figure")]
-    print(styles)
-    if all(characters[chara_pinyin] not in style.lower() for style in styles):
-        return await app.sendGroupMessage(
-            group,
-            MessageChain(
-                f"未找到角色{chara} | {chara_pinyin}！只查询到这几个呢（只能查到展柜里有的呢）："
-                f"{'、'.join([k for k, v in characters.items() if any(v in style.lower() for style in styles)])}"
-            ),
-            quote=source
-        )
-    else:
-        index = -1
-        chara_src = ""
-        for i, style in enumerate(styles):
-            if characters[chara_pinyin] in style.lower():
-                index = i
-                chara_src = style
-                break
-        if index == -1 or not chara_src:
-            return await app.sendGroupMessage(group, MessageChain("获取角色头像div失败！"))
-        await page.locator(f'div.avatar.svelte-188i0pk >> nth={index}').click()
-        _ = await page.locator('div.Card').wait_for()
-        buffer = await page.locator('div.Card').screenshot()
-        await page.close()
-        await app.sendGroupMessage(
-            group,
-            MessageChain([
-                f"use: {round(time.time() - start_time, 2)}s\n",
-                Image(data_bytes=buffer)
-            ]),
-            quote=source
-        )
+    try:
+        page = await browser.new_page()
+        await page.goto(url, wait_until="networkidle", timeout=100000)
+        await page.set_viewport_size({"width": 2560, "height": 1080})
+        await page.evaluate("document.getElementsByClassName('Dropdown-list')[0].children[13].dispatchEvent(new Event('click'));")
+        html = await page.inner_html(".CharacterList")
+        soup = BeautifulSoup(html, "html.parser")
+        styles = [figure["style"] for figure in soup.find_all("figure")]
+        print(styles)
+        if all(characters[chara_pinyin] not in style.lower() for style in styles):
+            await page.close()
+            await browser.close()
+            return await app.sendGroupMessage(
+                group,
+                MessageChain(
+                    f"未找到角色{chara} | {chara_pinyin}！只查询到这几个呢（只能查到展柜里有的呢）："
+                    f"{'、'.join([k for k, v in characters.items() if any(v in style.lower() for style in styles)])}"
+                ),
+                quote=source
+            )
+        else:
+            index = -1
+            chara_src = ""
+            for i, style in enumerate(styles):
+                if characters[chara_pinyin] in style.lower():
+                    index = i
+                    chara_src = style
+                    break
+            if index == -1 or not chara_src:
+                return await app.sendGroupMessage(group, MessageChain("获取角色头像div失败！"))
+            await page.locator(f'div.avatar.svelte-188i0pk >> nth={index}').click()
+            await page.locator('div.Card').wait_for()
+            # await page.locator('canvas.svelte-d1gpxk').wait_for()
+            await page.locator('img.WeaponIcon.svelte-gp6viv').wait_for()
+            # await page.locator('canvas.ArtifactIcon').wait_for()
+            buffer = await page.locator('div.Card').screenshot()
+            await page.close()
+            await browser.close()
+            await app.sendGroupMessage(
+                group,
+                MessageChain([
+                    f"use: {round(time.time() - start_time, 2)}s\n",
+                    Image(data_bytes=buffer)
+                ]),
+                quote=source
+            )
+    except:
+        await browser.close()
 
 
 async def init_chara_list():
