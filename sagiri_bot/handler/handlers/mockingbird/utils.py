2c240041e0d38473e82f83505d6385b314adc7bb
I Love Study <1450069615@qq.com>
2022-08-15 19:24:15 +0800

    Add condition for model_path not exist

--- sagiri_bot/handler/handlers/mockingbird/utils.py
+++ sagiri_bot/handler/handlers/mockingbird/utils.py
@@ -9,23 +9,26 @@ except ImportError:
     mockingbird_available = False
 
 model_path = Path.cwd() / "statics" / "models" / "mockingbird"
-models = [i.stem for i in model_path.iterdir() if i.is_dir()]
-
-try:
-    if mockingbird_available:
-        mockingbird = MockingBird()
-        mockingbird.load_model(
-            model_path / "encoder.pt",
-            model_path / "hifigan.pt"
-        )
-        current_model = "azusa"
-        mockingbird.set_synthesizer(model_path / current_model / f"{current_model}.pt")
-        generate_accuracy = 9
-        models_available = True
-    else:
-        models_available = False
-except FileNotFoundError:
+if not model_path.exists():
     models_available = False
+else:
+    models = [i.stem for i in model_path.iterdir() if i.is_dir()]
+
+    try:
+        if mockingbird_available:
+            mockingbird = MockingBird()
+            mockingbird.load_model(
+                model_path / "encoder.pt",
+                model_path / "hifigan.pt"
+            )
+            current_model = "azusa"
+            mockingbird.set_synthesizer(model_path / current_model / f"{current_model}.pt")
+            generate_accuracy = 9
+            models_available = True
+        else:
+            models_available = False
+    except FileNotFoundError:
+        models_available = False
 
 
 def get_voice(content: str):
